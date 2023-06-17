ce80a4bb1cd3af11125e7f060023980af85544b9
Igordevz <programadorigor.dev@gmail.com>
2023-06-17 20:10:53 -0300

    first commit

--- src/routes/configScreens.tsx
+++ src/routes/configScreens.tsx
@@ -2,8 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import InitialPage from '../screens/initialPage';
 import LoginScreen from '../screens/login';
-import { useEffect } from 'react';
-
 
 const Stack = createNativeStackNavigator();
 
