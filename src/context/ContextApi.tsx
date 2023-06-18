7fde851dde4b7acdcca81ea69cd0ba9ab5fb27a7
Igordevz <programadorigor.dev@gmail.com>
2023-06-18 11:48:32 -0300

    finalizado tela de inicio

--- src/context/ContextApi.tsx
+++ src/context/ContextApi.tsx
@@ -1,26 +1,27 @@
 import { createContext, useEffect, useState } from "react";
-import  { Iprops }  from "./protocolls"
+import { Iprops } from "./protocolls"
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Text, View } from "react-native";
 
 export const AuthContext = createContext({})
 
-export default function ProviderContext({children}: Iprops) {
-        
-    const [user, setUser] = useState<any | undefined>({});
+export default function ProviderContext({ children }: Iprops) {
 
-    useEffect(() => {
-        const GetUser = async () => {
-          const key: any | undefined = await AsyncStorage.getItem("user");
-          setUser(JSON.parse(key));
-        };
-    
-        GetUser();
-      }, []);
-   
-    return ( 
-        <AuthContext.Provider value={{ user }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  const [user, setUser] = useState<any | undefined>({});
+  useEffect(() => {
+
+    const GetUser = async () => {
+      const key: any | undefined = await AsyncStorage.getItem("user");
+      setUser(JSON.parse(key));
+    };
+
+    GetUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  )
 
 }
--- src/screens/initialPage/index.tsx
+++ src/screens/initialPage/index.tsx
@@ -8,28 +8,23 @@ export default function InitialPage({navigation}:any) {
   const { user }:any = useContext(AuthContext)
   
   if(user?.Token){
-    navigation.navigate("home")
+    navigation.navigate("home");
   }
-
   
   return (
     <>
-    <ScreenOne>
-      <Container>
-        <Image source={require("../../assets/Logoinitial.png")}/>
-        <Image source={require("../../assets/initial.png")} 
-        style={{width: 150, height: 150}}/>
-        <Text style={{fontSize: 30, color: "white"}}>Move with safety</Text>
-      </Container>
-      <ButtonNext onPress={() => navigation.navigate("login")}>
-         <Text style={{fontSize: 18, color: "white", fontWeight: "500", letterSpacing: 2}}>Get Started</Text>
-         <Image source={require("../../assets/seta-lado.png")}
-         style={{position: "absolute", right: 20}}/>
-      </ButtonNext>
-    </ScreenOne>
-
+      <ScreenOne>
+        <Container>
+          <Image source={require("../../assets/Logoinitial.png")}/>
+          <Image source={require("../../assets/initial.png")} 
+          style={{width: 150, height: 150}}/>
+          <Text style={{fontSize: 30, color: "white"}}>Move with safety</Text>
+        </Container>
+        <ButtonNext onPress={() => navigation.navigate("login")}>
+          <Text style={{fontSize: 18, color: "white", fontWeight: "500", letterSpacing: 2}}>Get Started</Text>
+          <Image source={require("../../assets/seta-lado.png")} style={{position: "absolute", right: 20}}/>
+        </ButtonNext>
+      </ScreenOne>
     </>
   )
-
-
 }
